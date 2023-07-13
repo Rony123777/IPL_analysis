8947a51cbc3b2c7036a598d1019968fdd53b149c
Rohan Bhoi <99428911+Rony123777@users.noreply.github.com>
2023-07-14 00:39:35 +0530

    Update ipl analysys.sql

--- ipl analysys.sql
+++ ipl analysys.sql
@@ -75,6 +75,16 @@ where deliveries.match_id=matches.id
 group by season,match_id,batting_team) as team_bounsaries
 group by season,batting_team;
 
+#Q9 WHAT IS THE HIGHEST PARTNERSHIP (RUNS) FOR EACH TEAM IN EACH SEASON?
+select season,batting_team,max(total_runs) as highest_partnership
+from(select season,batting_team,partnership,sum(total_runs) as total_runs
+from(select season,match_id,batting_team,over_no,
+sum(batsman_runs) as partnership,sum(batsman_runs)+sum(extra_runs) as total_runs
+from deliveries,matches where deliveries.match_id=matches.id
+group by season,match_id,batting_team,over_no) as team_scores
+group by season,batting_team,partnership) as highest_partnership
+group by season,batting_team; 
+
 #Q10 How many extra(wide and no ball) were bowled by each team in each season ?
 
 select m.id as match_no, d.batting_team ,
